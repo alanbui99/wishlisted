c3524f77813b0b1d60c4f104c9748e145d69ca92
Thang Bui <minhthang200399@gmail.com>
2021-01-15 02:52:56 -0500

    ui changes and others

--- tracker_app/forms.py
+++ tracker_app/forms.py
@@ -1,41 +1,24 @@
 from django import forms
 from .models import Item
 from urllib.parse import urlparse
-
-# class ItemForm(forms.ModelForm):
-#     class Meta:
-
-#         model = Item
-#         fields = ('url', 'email', 'notify_when', 'desired_price')
-#         labels = {
-#             'url': ('Amazon URL'),
-#             'notify_when': ('Notify when price'),
-#             'desired_price': ('Notify when price is below')
-#         }
-#         widgets = {
-#             'url': forms.URLInput(attrs={'class': 'form-control mb-4', 'placeholder':'https://www.amazon.com/...'}),
-#             'email': forms.EmailInput(attrs={'class': 'form-control mb-4'}),
-#             'notify_when': forms.RadioSelect(choices=NOTIFY_CHOICES, attrs={'class': 'list-unstyled mx-3', 'onchange': "configDisplay();"}),
-#             'desired_price': forms.NumberInput(attrs={'class': 'form-control'}),
-#         }
-
 class ItemForm(forms.Form):
     NOTIFY_CHOICES = [
         ('below', 'is below a certain price'),
         ('down', 'goes down'),
         ('change', 'changes'),
+        ('no', 'do not notify')
     ]
 
     url = forms.URLField(label='Amazon URL', widget=forms.URLInput(attrs= {
-        'class': 'form-control mb-4',
+        'class': 'form-control',
         'placeholder': 'https://www.amazon.com/...'
     }))
 
     email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs= {
-        'class': 'form-control mb-4'
+        'class': 'form-control'
     }))
 
-    notify_when = forms.CharField(widget=forms.RadioSelect(choices=NOTIFY_CHOICES,  attrs= {
+    notify_when = forms.CharField(label='Notify when price', widget=forms.RadioSelect(choices=NOTIFY_CHOICES,  attrs= {
         'class': 'list-unstyled', 
         'onchange': 'displayDesiredPrice();'
     }), 
