3dab7b7a4a136ad60ef82ba28876b50b5f1d9b52
Thang Bui <minhthang200399@gmail.com>
2021-07-06 02:06:57 -0400

    environ []

--- wishlisted/settings.py
+++ wishlisted/settings.py
@@ -21,7 +21,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEV", True)
 
